d24989d49c32b5099d370aa97df2b1f67ff14787
Sam <2102926@talnet.nl>
2020-05-11 02:50:39 +0200

    Uitwerking van de exercise

--- queries.sql
+++ queries.sql
@@ -1,10 +1,39 @@
-use netland;
-
-select title from series where has_won_awards = 1;
-select title from series where rating > 2.5;
-select title from series where country = 'NL' AND language = 'NL';
-select title from series where seasons < 5;
-select title from series order by rating desc;
-select title from series where seasons < 3 || seasons > 20;
-select title from series where title like '%Th%';
-select title from series where seasons != 3;
+SELECT title 
+FROM netland.series;  
+SELECT * FROM 
+netland.series 
+WHERE rating>2.5;
+SELECT * FROM 
+netland.series 
+WHERE has_won_award;
+SELECT * FROM 
+netland.series
+WHERE language ='nl' ;
+SELECT * FROM 
+netland.series
+WHERE seasons<5 ;
+SELECT * FROM 
+netland.series
+order by rating desc;
+SELECT * FROM 
+netland.series WHERE Seasons>3 or <20 ; 
+SELECT * FROM netland.series
+ WHERE Seasons<3 ||seasons >20 ; 
+SELECT * FROM 
+netland.series
+WHERE title like '%Th%' ;
+SELECT * FROM 
+netland.series
+WHERE seasons !=3 ;
+SELECT  * FROM 
+netland.series 
+WHERE rating>2.5 
+order by rating desc;
+SELECT * FROM 
+netland.series 
+WHERE seasons<5 
+order by seasons desc; 
+SELECT * FROM 
+netland.series 
+WHERE Seasons<3 ||seasons >20 
+order by country ASC;
